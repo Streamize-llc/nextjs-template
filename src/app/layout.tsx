a10d4c998a9addafe5449f106463a9e8d117fde4
SlZeroth <qkrwncks593@gmail.com>
2025-01-18 17:45:58 +0900

    Integrate Google Analytics and update dependencies
    - Added `@next/third-parties` package for Google Analytics integration in `layout.tsx`.
    - Updated `package.json` and `package-lock.json` to include `@next/third-parties` and its dependencies.
    - Enhanced project tracking capabilities with the addition of Google Analytics component.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SupabaseProvider from "@/utils/supabase/provider";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GAID!} />
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
